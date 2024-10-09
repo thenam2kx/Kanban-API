7091c86bb148d0420ba0711b538f542b8a0c891a
thenam2kx <thenam2kx@example.com>
2024-10-10 04:43:26 +0700

    sigin-user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -64,3 +64,62 @@ export const register = async (req: any, res: any) => {
     })
   }
 }
+
+
+export const signin = async (req: any, res: any) => {
+  const body = req.body
+  const { email, password } = body
+
+  try {
+    // Validate username format
+    // if (!validateUsername(username)) {
+    //   return res.status(400).json({ message: 'Username must be 3-20 characters long and contain only alphanumeric characters' });
+    // }
+
+    // Validate password strength
+    // if (!validatePassword(password)) {
+    //   return res.status(400).json({
+    //     message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+    //   });
+    // }
+
+    // Check if user exists
+    const user: any = await userModel.findOne({email})
+    if (user) {
+      throw new Error('Account not found. Please check username or password!')
+    }
+
+    console.log(user)
+
+
+    // One-way encryption password
+    // const salt = await bcrypt.genSalt(10)
+    // const hashpassword = await bcrypt.hash(password, salt)
+    // body.password = hashpassword
+
+
+    // Save data to database
+    // const newUser = new userModel(body)
+    // await newUser.save()
+
+    // Optionally delete password before returning response
+    // delete newUser.password
+    const { password: _, ...userWithoutPassword } = user.toObject();
+
+
+    res.status(200).json({
+      message: 'Login successful',
+      data: {...userWithoutPassword, token: await getAccessToken({
+        _id: userWithoutPassword._id,
+        email: userWithoutPassword.email as string,
+        rule: userWithoutPassword.rule ?? 1
+      })}
+    })
+
+
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
+}
